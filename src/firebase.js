89642e624c846c4406b28d8abb4f8d7fffeb0bb3
descifle <natashashanae94@gmail.com>
2022-12-03 08:57:07 -0500

    initialized google firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQqMbfGJULRYbz75Czr-10my9AoBj5P_s",
+  authDomain: "animal-welfare-tracker.firebaseapp.com",
+  projectId: "animal-welfare-tracker",
+  storageBucket: "animal-welfare-tracker.appspot.com",
+  messagingSenderId: "899385075966",
+  appId: "1:899385075966:web:6c83c5f2e02ba0ff6a5daf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
