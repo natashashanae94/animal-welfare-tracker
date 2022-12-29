34c6feab2418be6f295e20423db4e8420811b625
Natasha Johnson <JohnsonN@si.edu>
2022-12-29 11:51:38 -0500

    pushing up changes

---
+++ src/animalListSource.js
@@ -0,0 +1,19 @@
+const animalColumns = [
+    {
+        field: 'accessionNumber',
+        headerName: 'Accession Number',
+        width: 150,
+        description:
+            'The number that idenitifies the animal.',
+    },
+    { field: 'name', headerName: 'Name' },
+    { field: 'gender', headerName: 'Gender' },
+    {
+        field: 'assessor',
+        headerName: 'Assessed By',
+        width: 150,
+        description:
+            'The name of the assessor.',
+    },
+    { field: 'group', headerName: 'Group' },
+];
--- src/components/AnimalList.jsx
+++ src/components/AnimalList.jsx
@@ -1,4 +1,5 @@
 import Box from '@mui/material/Box';
+import {animalColumns} from "../animalListSource";
 import { DataGrid } from '@mui/x-data-grid';
 import { Link } from "react-router-dom";
 
@@ -11,111 +12,54 @@ import {
     onSnapshot,
 } from "firebase/firestore";
 import { db } from "../firebase";
+import { ref } from 'firebase/storage';
 
 const AnimalList = () => {
-    const [data, setData] = useState([]);
+    const [animal, setAnimal] = useState([]);
+    const [loading, setLoading] = useState([]);
+
+    function getAnimals() {
+        setLoading(true);
+        ref.onSnapshot((querySnapshot) => {
+            const items = [];
+            querySnapshot.forEach((doc) => {
+                items.push(doc.data());
+            });
+            setSchools(items);
+            setLoading(false);
+        });
+    }
 
     useEffect(() => {
-        // const fetchData = async () => {
-        //   let list = [];
-        //   try {
-        //     const querySnapshot = await getDocs(collection(db, "animals"));
-        //     querySnapshot.forEach((doc) => {
-        //       list.push({ id: doc.id, ...doc.data() });
-        //     });
-        //     setData(list);
-        //     console.log(list);
-        //   } catch (err) {
-        //     console.log(err);
-        //   }
-        // };
-
-        // fetchData();      
-
-        // LISTEN (REALTIME)
-        const unsub = onSnapshot(
-            collection(db, "animals"),
-            (snapShot) => {
-                let list = [];
-                snapShot.docs.forEach((doc) => {
-                    list.push({ id: doc.id, ...doc.data() });
-                });
-                setData(list);
-            },
-            (error) => {
-                console.log(error);
-            }
-        );
-
-        return () => {
-            unsub();
-        };
+        getAnimals();
     }, []);
 
-    // Animal List Column Names
-    const columns = [
-        {
-            field: 'accession number',
-            headerName: 'Accession Number',
-            width: 150,
-            description:
-                'The number that idenitifies the animal.',
-        },
-        { field: 'name', headerName: 'Name' },
-        { field: 'gender', headerName: 'Gender' },
-        {
-            field: 'assessor',
-            headerName: 'Assessed By',
-            width: 150,
-            description:
-                'The name of the assessor.',
-        },
-        { field: 'group', headerName: 'Group' },
-    ];
-
-
-    const handleDelete = async (id) => {
-        try {
-            await deleteDoc(doc(db, "animals", id));
-            setData(data.filter((item) => item.id !== id));
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
-    const actionColumn = [
-        {
-            field: "action",
-            headerName: "Action",
-            width: 200,
-            renderCell: (params) => {
-                return (
-                    <div className="cellAction">
-                        <Link to="/users/test" style={{ textDecoration: "none" }}>
-                            <div className="viewButton">View</div>
-                        </Link>
-                        <div
-                            className="deleteButton"
-                            onClick={() => handleDelete(params.row.id)}
-                        >
-                            Delete
-                        </div>
-                    </div>
-                );
-            },
-        },
-    ];
+    if (loading) {
+        return <h1>Loading...</h1>;
+    }
 
     return (
-        <Box sx={{ height: 400, width: '100%' }}>
-            <DataGrid
-                className="datagrid"
-                rows={data}
-                columns={columns.concat(actionColumn)}
-                pageSize={9}
-                rowsPerPageOptions={[9]}
-            />
-        </Box>
+        <React.Fragment>
+            <div>
+                {animal.map((data) => (
+                    <>
+                        <h2>{data.name}</h2>
+                        <p>{data.gender}</p>
+                        <p>{data.species}</p>
+                        <p>{data.group}</p>
+                    </>
+                ))}
+            </div>
+        </React.Fragment>
+        // <Box sx={{ height: 400, width: '100%' }}>
+        //     <DataGrid
+        //         className="datagrid"
+        //         rows={animal}
+        //         columns={animalColumns}
+        //         pageSize={9}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+        //         rowsPerPageOptions={[9]}
+        //     />
+        // </Box>
     );
 }
 
